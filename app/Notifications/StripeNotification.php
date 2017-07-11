625bc6c49dce6e9085a606062937a882869d9d84
Jyrone Parker <jyrone.parker@gmail.com>
2017-07-11 15:17:17 -0400

    typo

--- app/Notifications/StripeNotification.php
+++ app/Notifications/StripeNotification.php
@@ -52,7 +52,7 @@ class StripeNotification extends Notification
             // ->id($notification->id)
             ->title('New Stripe Notification - '. $this->type)
             ->icon(url('/push.png'))
-            ->body("$".money_format('%.2n', $this->data["amount"]/100))
+            ->body("$".money_format('%.2n', $this->data["amount"]/100));
     }
 
 
