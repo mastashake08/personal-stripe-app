745e5b097b060c4b1f6ae69229b592deb97274eb
Jyrone Parker <jyrone.parker@gmail.com>
2017-07-11 14:32:34 -0400

    vibrate

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -3,7 +3,8 @@ self.addEventListener('push', function(event) {
     var data = event.data.json();
     self.registration.showNotification(data.title,{
       body: data.body,
-      icon: data.icon
+      icon: data.icon,
+      vibrate: [300]
     });
     console.log('This push event has data: ', event.data.text());
   } else {
