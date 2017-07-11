8403955ad893718649ea5e1e4ce4a5c1e70dbc10
Jyrone Parker <jyrone.parker@gmail.com>
2017-07-11 15:00:44 -0400

    typo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
-        App\Console\Commands\GetStripeBalance::class,
+        Commands\GetStripeBalance::class,
     ];
 
     /**
