c08313a2477659226b7b794e460711543fb7d233
Jyrone Parker <jyrone.parker@gmail.com>
2017-07-11 14:34:37 -0400

    typo

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Events\StripeNotification;
+use App\Notifications\StripeNotification;
 class WebhookController extends Controller
 {
     //
