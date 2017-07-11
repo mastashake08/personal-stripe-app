19774ad370e5688f030be894291ef27ab624660c
Jyrone Parker <jyrone.parker@gmail.com>
2017-07-11 14:38:32 -0400

    money format

--- app/Notifications/StripeNotification.php
+++ app/Notifications/StripeNotification.php
@@ -44,7 +44,7 @@ class StripeNotification extends Notification
             // ->id($notification->id)
             ->title('New Stripe Notification - '. $this->type)
             ->icon(url('/push.png'))
-            ->body($this->data["amount"])
+            ->body(money_format('%.2n', $this->data["amount"]/100))
             ->action('View account', 'view_account');
     }
 
