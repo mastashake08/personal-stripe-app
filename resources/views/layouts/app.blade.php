e2378d86a95b62f4b11245f2ac4d547c330d67e9
Jyrone Parker <jyrone.parker@gmail.com>
2017-11-19 13:29:18 -0500

    standalone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,6 @@
 <html lang="{{ config('app.locale') }}">
 <head>
   <link rel="manifest" href="manifest.json">
-
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="msapplication-starturl" content="/">
